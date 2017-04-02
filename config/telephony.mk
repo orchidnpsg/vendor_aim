89c33a5df3275149221bd17391ecac3b2c8b1a62
ShadowReaper1 <androgv@gmail.com>
2017-04-02 19:19:42 +0530

    vendor: Rework Rebranded by GreekDragon Changelog Generator by Anand

--- config/telephony.mk
+++ config/telephony.mk
@@ -4,7 +4,7 @@ PRODUCT_COPY_FILES += \
 
 # World APN list
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/aim/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \
