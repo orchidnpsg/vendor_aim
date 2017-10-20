8fb0364ed6073f1253bc234f5aa8ea972caa2892
Anand Ramachandran <anand.nv1998@gmail.com>
2017-10-20 19:29:16 +0530

    Fix soong flag and add missing flags that we removed while readding
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,10 +1,15 @@
-package android
+package android_aim
 type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
+	}
+
+	Uses_non_treble_camera struct {
+		Cflags []string
 	}
 }
 
 type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
+	Uses_non_treble_camera  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,7 +3,8 @@ aim_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
-	echo '"AIM": {'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
+	echo '"Aim": {'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
