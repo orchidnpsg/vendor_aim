1027c05953748b4450993f1f30873137f586988f
Kshitij Gupta <kshitijgm@gmail.com>
2018-05-25 19:58:35 +0000

    vendor: Fix colors on the stock base overlay
    - Also leave stock in (with some values fixed) to let the user choose.
    - Call the fixed up overlay "Stocktato" because we can ;)
    
    Change-Id: I32458f21eb0493708cb0528580144ae8c4207eb4
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- config/aim_extras.mk
+++ config/aim_extras.mk
@@ -60,7 +60,8 @@ PRODUCT_PACKAGES += \
      PixelTheme \
      StockTheme \
      PixelBaseTheme \
-     StockBaseTheme
+     StockBaseTheme \
+     StockBaseFixedTheme
 
 # DarkUI Packages
  PRODUCT_PACKAGES += \
