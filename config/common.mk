5b23700af89ad34c7fa4f0800e7510c6cffcaea3
Rpingat <pingatravi@gmail.com>
2020-03-26 13:24:05 +0530

    vendor/aim: fix version properties
    Change-Id: I889db0bba32b518252c4bf59ca5ed497a7479454

--- config/common.mk
+++ config/common.mk
@@ -1,8 +1,6 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 
-PRODUCT_BRAND ?= LineageOS
-
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
